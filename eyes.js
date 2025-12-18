138d5ee269c82a595cd355cbb6e665c02baa8537
tdavande <113100339+tdavande@users.noreply.github.com>
2025-12-18 18:42:10 +0000

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,3 +1,7 @@
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+// by Zach Krall
+// http://zachkrall.online/
+
 osc( 215, 0.1, 2 )
 .modulate(
   osc( 2, -0.3, 100 )
@@ -7,14 +11,14 @@ osc( 215, 0.1, 2 )
   osc( 215, -0.1, 2)
   .pixelate( 50, 50 )
 )
-.color( 0.9, 0.0, 0.9 )
+.color( 0, 1, 0 )
 .modulate(
   osc( 6, -0.1 )
   .rotate( 9 )
 )
 .add(
   osc( 10, -0.9, 900 )
-  .color(1,0,1)
+  .color(0,1,0)
 )
 .mult(
   shape(900, 0.2, 1)
@@ -32,3 +36,4 @@ osc( 215, 0.1, 2 )
   .color(0.2,0,1)
 )
 .out()
+
